4eb9ca028043dd9aa1589db81a6a2c9cfcb94e67
SFboina <boinasoifaou@gmail.com>
2025-06-01 11:54:26 +0200

    navigation

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,7 +6,6 @@ import Navigation from '../components/Navigation';
 const Error = () => { 
   return (
     <>
-      <Navigation />
     <div className="error">
       <h1 className="error-title">404</h1>
       <p className="error-text">Oups ! La page que vous demandez n'existe pas.</p>
